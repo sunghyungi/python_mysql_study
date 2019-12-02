2f3a5a4f2dcef56e9f4f7269ee4ea589dc899d6d
hyungi <tjdgusrlek@gmail.com>
2019-12-02 12:15:01 +0900

    connection_pool

---
+++ connection_pool/coneection_pool_study02.py
@@ -0,0 +1,28 @@
+import mysql
+from mysql.connector.pooling import MySQLConnectionPool
+
+
+class ExplicitlyConnectionPool(object):
+    INSTANCE = None
+
+    def __init__(self):
+        if self.INSTANCE is not None:
+            raise ValueError("An instantiation already exists!")
+        else:
+            self.__cnxPool = MySQLConnectionPool(pool_name="myPool", pool_size=5, option_files='db_config.conf')
+
+    @classmethod
+    def get_instance(cls):
+        if cls.INSTANCE is None:
+            cls.INSTANCE = ExplicitlyConnectionPool()
+        return cls.INSTANCE
+
+    def get_connection(self):
+        return self.__cnxPool.get_connection()
+
+    @classmethod
+    def pool_close(cls):
+        cls.INSTANCE = None
+
+def get_implicitly_connection():
+    return MySQLConnectionPool(option_files='db_config.conf', pool_name="myPool", pool_size=5)
--- connection_pool/connection_pool_main.py
+++ connection_pool/connection_pool_main.py
@@ -1,5 +1,6 @@
 import inspect
 
+from connection_pool.coneection_pool_study02 import ExplicitlyConnectionPool, get_implicitly_connection
 from connection_pool.connection_pool_study import DatabaseConnectionPool
 
 
@@ -10,6 +11,28 @@ def connection_pool01():
     connection.close()
 
 
+def explicitly_connection_pool():
+    print("== {}() ==".format(inspect.stack()[0][3]))
+    connectionPool = ExplicitlyConnectionPool.get_instance()
+    connection = connectionPool.get_connection()
+    print(type(connection), connection)
+    connection.close()
+
+
+def implicitly_connection_pool():
+    print("== {}() ==".format(inspect.stack()[0][3]))
+    connectionPool = get_implicitly_connection()
+    connection = connectionPool.get_connection()
+    print(type(connection), connection)
+    connection.close()
+
+
 if __name__ == "__main__":
-    for i in range(20):
-        connection_pool01()
+    # for i in range(20):
+    #     connection_pool01()
+
+    explicitly_connection_pool()
+    implicitly_connection_pool()
+
+    explicitly_connection_pool()
+    implicitly_connection_pool()
